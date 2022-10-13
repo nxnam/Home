1fa5d0a6809f8f23c22d2ccbe855e1a2da219c7d
Nam <nxnamvn@gmail.com>
2022-10-13 15:06:53 +0700

    update code

--- Sources/Home/HomeBuilder.swift
+++ Sources/Home/HomeBuilder.swift
@@ -20,17 +20,17 @@ class HomeComponent: Component<HomeDependency> {
 
 // MARK: - Builder
 
-protocol HomeBuildable: Buildable {
+public protocol HomeBuildable: Buildable {
     func build(withListener listener: HomeListener) -> HomeRouting
 }
 
-final class HomeBuilder: Builder<HomeDependency>, HomeBuildable {
+open class HomeBuilder: Builder<HomeDependency>, HomeBuildable {
 
     override init(dependency: HomeDependency) {
         super.init(dependency: dependency)
     }
 
-    func build(withListener listener: HomeListener) -> HomeRouting {
+    public func build(withListener listener: HomeListener) -> HomeRouting {
         let component = HomeComponent(dependency: dependency)
         let viewController = HomeViewController()
         let interactor = HomeInteractor(presenter: viewController)
